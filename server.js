ca39132a4e7e3d58855925d9b704795e04b5588e
Sylvia <smora1992@gmail.com>
2021-04-02 17:41:56 +0400

    adding return statement to the arrow functions

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const MOVIES = require('./movies.json')
 
 const app = express()
 
+
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors());
@@ -27,30 +28,65 @@ app.use(function validateBearerToken(req, res, next) {
 
 app.get('/movie', function movieHandler(req, res) {
     let response = MOVIES;
+    const { genre, country, avg_vote } = req.query;
+
+
+    // const genreResponse = response.filter((movie) => {
+    //     if (!genre) {
+    //         return movie;
+    //     }
+    //     return movie.genre.trim().toLowerCase() === decodeURI(genre).toLowerCase().trim()
+    // })
+
+    // const countryResponse = genreResponse.filter((movie) => {
+    //     if (!country) {
+    //         return movie;
+    //     }
+    //     return movie.country.trim().toLowerCase() === decodeURI(country).toLowerCase().trim()
+    // });
+
+    // const averageVoteResponse = countryResponse.filter((movie) => {
+    //     if (!avg_vote) {
+    //         return movie;
+    //     }
+    //     return Number(movie.avg_vote) >= Number(avg_vote.trim())
+    // });
+
+    // return res.status(200)
+    //     .json(averageVoteResponse)
 
     if (req.query.genre) {
-        console.log(req.query.genre)
-        console.log(response[0].genre)
+
         response = response.filter(movie => {
-            movie.genre.toLowerCase().includes(req.query.country.toLowerCase())
+
+            return movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
 
         })
+
     }
     if (req.query.country) {
+
         response = response.filter(movie => {
-            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+            return movie.country.toLowerCase().includes(req.query.country.toLowerCase())
         })
+
     }
     if (req.query.avg_vote) {
         response = response.filter(movie => {
-            Number(movie.avg_vote) >= Number(req.query.avg_vote)
+            return Number(movie.avg_vote) >= Number(req.query.avg_vote)
         })
+
     }
     console.log(response)
     res.json(response).send();
 })
 
+
 const PORT = 6000
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
